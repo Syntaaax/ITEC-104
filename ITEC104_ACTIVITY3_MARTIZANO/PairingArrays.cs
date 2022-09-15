e0fba7d78291f6b6bcabe82e7243b273c6cfead3
Francine <112456101+Syntaaax@users.noreply.github.com>
2022-09-15 16:21:08 +0800

    Add files via upload

---
+++ ITEC104_ACTIVITY3_MARTIZANO/PairingArrays.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace PairingArrays {
+    class Program{
+        static void Main(string[] args) {
+
+            String[] credentials = new String[]{"fmartizano@gmail.com", "Francine", "Syntaaax"};
+
+            Console.WriteLine("Email: " + credentials[0]);
+            Console.WriteLine("Username: " + credentials[1]);
+            Console.WriteLine("Password: " + credentials[2]);
+
+        }
+    }
+}
