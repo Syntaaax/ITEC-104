3157e7c2ab5068b49537b2faa47b1af765d35519
Francine <112456101+Syntaaax@users.noreply.github.com>
2022-10-07 19:07:51 +0800

    Add files via upload

---
+++ ITEC104_ACTIVITY5_MARTIZANO/List.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List {
+
+    static void Main(string[] args) {
+
+        List<int> myList = test(new List<int> (new int[] {10, 20, 35, 65, 53, 48, 5, 1}));
+
+        Console.WriteLine("10 20 35 65 53 48 5 1");
+        foreach(var i in myList)
+        {
+            Console.Write(i.ToString() + " ");
+        }
+    }
+
+    public static List<int> test(List<int> nums)
+    {
+        return nums.Where(n => n % 10 < 5).ToList();
+    }
+}
